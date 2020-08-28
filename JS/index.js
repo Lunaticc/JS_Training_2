b516d437049cc2336249c5af786098b0605fbf0f
David <David.thiman@gmail.com>
2020-08-28 21:40:42 +0200

    Initiation and some JS

---
+++ JS/index.js
@@ -0,0 +1,22 @@
+console.log('Fuck this shit, Im out!');
+var x = 10;
+var y = 'Fuck this shit, Im out!';
+const author = 'David Thiman';
+let identity = 'Name';
+let name;
+name = 'Fufufufuf';
+console.log(name);
+
+const insterestRate = 0.3;
+// insterestRate = 0.9;
+name = 'fhhhf';
+console.log(name);
+
+
+const n = function(){};
+for(let i = 0;i<20;i++){
+        if(x >= 10){
+        console.log(x * i);
+    }
+}
+console.log(author +" " + x);
