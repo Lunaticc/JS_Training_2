4a60794fe77f1ca30b5a51778d8bab6bf5cded11
David <David.thiman@gmail.com>
2020-08-29 04:28:01 +0200

    More JS

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,36 @@
 console.log('Fuck this shit, Im out!');
+let element = document.getElementById("index.html");
+// let variantion = document.querySelector(h1);
+// let el = element.getElementsByClassName('hee');
+let violation = document.getElementsByClassName('findText');
+let person = {
+    name: "David",
+    age: {
+        days: 54,
+        years: 27
+    }
+};
+
+function area(number, number2){
+    return number * number2;
+}
+
+let gerr = area(5, 3);
+console.log(gerr);
+// let selectedColors = ['Red', 'Green', 'Blue'];
+// selectedColors[3] = 'Yellow';
+// console.log(selectedColors);
+// person.name = 'Greg';
+function greet(name) {
+    console.log('Yellow');
+    console.log('Stop ' + name);
+}
+greet(violation);
+//bracket noration
+let selection = 'name';
+person[selection] = 'Mary';
+
+console.log(person);
 var x = 10;
 var y = 'Fuck this shit, Im out!';
 const author = 'David Thiman';
@@ -13,10 +45,11 @@ name = 'fhhhf';
 console.log(name);
 
 
-const n = function(){};
-for(let i = 0;i<20;i++){
-        if(x >= 10){
-        console.log(x * i);
-    }
-}
+// const n = function(){};
+// for(let i = 0;i<20;i++){
+//         if(x >= 10){
+//         console.log(x * i);
+//     }
+// }
 console.log(author +" " + x);
+
